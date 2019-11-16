79644490fe6457abe99f7474c636523e771c2583
kabin.sillwatwinyoo@ktb.co.th <kabin.sillwatwinyoo@ktb.co.th>
2019-11-16 15:05:36 +0700

    test http url and get method

--- src/app/services/income/income.service.spec.ts
+++ src/app/services/income/income.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IncomeService } from './income.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('IncomeService', () => {
   let service: IncomeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(IncomeService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call method get with url get income api', () => {
+    service.getIncomeByUserId().subscribe();
+    const req = httpTestingController.expectOne(
+      'http://103.74.254.157:9003/income/id/1'
+    );
+    expect(req.request.method).toBe('GET');
   });
 });
